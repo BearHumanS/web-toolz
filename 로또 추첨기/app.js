8c50296aae008498d7264f338df6e714ddb105bc
BearHumanS <bye0651@naver.com>
2023-03-01 16:00:14 +0900

    first commit

--- 로또 추첨기/app.js
+++ 로또 추첨기/app.js
@@ -2,16 +2,19 @@ const start = document.querySelector("#start");
 const result = document.querySelector("#result");
 
 function startLotto() {
-    const lottoNumbers = [];
-    while(lottoNumbers.length < 6){
-    const lottoNumber = Math.floor(Math.random()*45)+1;
-    if(!lottoNumbers.includes(lottoNumber)) {
-        lottoNumbers.push(lottoNumber);
-        };
-    };
-    lottoNumbers.sort(function(a, b){return a-b});
+  const lottoNumbers = [];
+  while (lottoNumbers.length < 6) {
+    const lottoNumber = Math.floor(Math.random() * 45) + 1;
+    if (!lottoNumbers.includes(lottoNumber)) {
+      lottoNumbers.push(lottoNumber);
+    }
+  }
 
-    result.innerHTML = lottoNumbers.join(",");
-};
+  lottoNumbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  result.innerHTML = lottoNumbers;
+}
 
 start.addEventListener("click", startLotto);
